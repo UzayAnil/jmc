ad6df16d94ddecde5373a3e0f79e0b03c3d0c48d
Felix posey <34116619+whitetrashyt@users.noreply.github.com>
2020-03-28 14:20:14 -0400

    copy to ubuntu server

---
+++ file.py
@@ -0,0 +1,27 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
+import random as rand
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+# hate this shit
+
+GPIO.setup(6, GPIO.OUT)
+GPIO.SETUP(26, GPIO.IN)
+voltstatus = GPIO.input(26)
+GPIO.output(6, GPIO.LOW)
+p = GPIO.PWM(6, 1)
+freq = 1
+p.start(freq)
+while 0 < 1:
+    try:
+        if voltstatus == 1:
+            freq = rand.randint(1, 6)
+        else:
+
+            GPIO.output(6, 0)
+            print "python's fucked"
+    except KeyboardInterrupt:
+        GPIO.cleanup()
